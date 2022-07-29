4928566e0798af737bf63217a869b28ad78362d9
yang-21020263 <21020263@myrp.edu.sg>
2022-07-29 13:05:54 +0800

    created first page layout

--- app/src/main/java/sg/edu/rp/c346/id21020263/mymovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21020263/mymovies/MainActivity.java
@@ -3,12 +3,29 @@ package sg.edu.rp.c346.id21020263.mymovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTitle, etGenre, etYear;
+    Button btnInsert, btnList;
+    Spinner ratings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTitle = findViewById(R.id.editTextTitle);
+        etGenre = findViewById(R.id.editTextGenre);
+        etYear = findViewById(R.id.editTextYear);
+        btnInsert = findViewById(R.id.buttonInsert);
+        btnList = findViewById(R.id.buttonList);
+        ratings = findViewById(R.id.spinnerRating);
+
+
     }
 }
